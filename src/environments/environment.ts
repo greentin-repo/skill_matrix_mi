802b07e30af4fc505b471bc7e7535854fa6d791c
Rushikesh Jagtap <imrushij@gmail.com>
2025-05-16 18:19:56 +0530

    env: Update production API URL to include '/spicer/' for consistency

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   // apiUrl: 'http://gtdev.greentinsolutions.com:8085/spicer/' //Dev
   // apiUrl: 'http://10.37.61.251:8080/enovation-prod/'   //Prod
-  apiUrl: 'http://91.108.110.98:8080/',
+  apiUrl: 'http://91.108.110.98:8080/spicer/', //Prod
   // apiUrl: "http://localhost:8091/",
 };
 /*
