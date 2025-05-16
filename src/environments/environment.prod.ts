19f116af3bef59637e81489c818a5905c27d9754
Rushikesh Jagtap <imrushij@gmail.com>
2025-05-16 09:58:17 +0530

    env: Comment out old API URLs and ensure the correct API URL is set for both production and development environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://gtdev.greentinsolutions.com:8085/spicer/', //Dev
+  // apiUrl: 'http://gtdev.greentinsolutions.com:8085/spicer/', //Dev
   // apiUrl: 'http://10.37.61.251:8080/enovation-prod/',   //Prod
+  apiUrl: 'http://91.108.110.98:8080/',   //
   // apiUrl: "http://localhost:8091/",
   version: '1.0.8'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://gtdev.greentinsolutions.com:8085/spicer/' //Dev
+  // apiUrl: 'http://gtdev.greentinsolutions.com:8085/spicer/' //Dev
   // apiUrl: 'http://10.37.61.251:8080/enovation-prod/'   //Prod
+  apiUrl: 'http://91.108.110.98:8080/',
   // apiUrl: "http://localhost:8091/",
 };
 /*
